6e70eedc245d49a86e15b339915e4362cbb741e7
Jannesh Rao <janneshr007@gmail.com>
2026-02-07 11:06:02 +0530

    Add: JsonPropertyOrder for CustomerDTO

--- src/main/java/com/jannesh/dto/customer/CustomerDTO.java
+++ src/main/java/com/jannesh/dto/customer/CustomerDTO.java
@@ -1,13 +1,16 @@
 package com.jannesh.dto.customer;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.jannesh.util.enums.CustomerStatus;
 import com.jannesh.util.enums.Gender;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter @Setter
+@JsonPropertyOrder({"customerId","fullName","mobileNumber","email","gender","customerStatus","createdAt","updatedAt"})
 public class CustomerDTO {
     private UUID customerId;
     private String fullName;
@@ -15,4 +18,6 @@ public class CustomerDTO {
     private String email;
     private Gender gender;
     private CustomerStatus customerStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
